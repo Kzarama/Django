8862b4e1919613f1cca7cf8945339b171730ad1d
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-05-22 22:55:08 -0500

    instagram initiated

---
+++ Instagram/posts/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from datetime import datetime
+
+posts = [
+    {
+        'name': 'Mont blanc',
+        'user': 'Yesica',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036'
+    },
+
+    {
+        'name': 'Via lactea',
+        'user': 'Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903'
+    },
+
+    {
+        'name': 'Auditorio',
+        'user': 'Naranjo',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076'
+    },
+]
+
+def list_posts(request):
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><strong>{user} - <i>{timestamp}</i></strong></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
