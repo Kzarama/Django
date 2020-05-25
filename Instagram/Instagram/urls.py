d49039cc63ddd3946fd0b827003bdf2d44d0ea67
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-05-24 21:08:04 -0500

    path to signup added

--- Instagram/Instagram/urls.py
+++ Instagram/Instagram/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
 
     path('users/login/', users_views.login_view, name='login'),
     path('users/logout/', users_views.logout_view, name='logout'),
+    path('users/signup/', users_views.signup, name='signup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
