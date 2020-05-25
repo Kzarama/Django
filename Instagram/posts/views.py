d39b048b7ab31a5f7aa6653c32d19019bd106680
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-05-25 10:21:08 -0500

    method for create post added and deleted default posts

--- Instagram/posts/views.py
+++ Instagram/posts/views.py
@@ -1,47 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from posts.forms import PostForm
 
-from datetime import datetime
-
-posts = [
-    {
-        'title': 'Flowers',
-        'user': {
-            'name': 'Nicole',
-            'picture': 'https://picsum.photos/60/60/?image=1027'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/600?image=152',
-    },
-    {
-        'title': 'Love bridge',
-        'user': {
-            'name': 'Kevin Zarama',
-            'picture': 'https://picsum.photos/60/60/?image=338'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/id/336/600/600',
-    },
-    {
-        'title': 'Dogs',
-        'user': {
-            'name': 'Christian Pechene',
-            'picture': 'https://picsum.photos/60/60/?image=1005'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/id/237/600/600',
-    },
-    {
-        'title': 'Nature',
-        'user': {
-            'name': 'Natalia Burbano',
-            'picture': 'https://picsum.photos/60/60/?image=883'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/500/700/?image=161',
-    }
-]
+from posts.models import  Post
 
 @login_required
 def list_posts(request):
+    posts = Post.objects.all().order_by('-created')
     return render(request, 'posts/feed.html', {'posts':posts})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+    return render(
+        request=request,
+        template_name='posts/new.html',
+        context={
+            'form':form,
+            'user':request.user,
+            'profile':request.user.profile
+        }
+    )
