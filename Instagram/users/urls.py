306f8915733382439089888ab1c7fed99d50b0d0
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-05-27 22:51:24 -0500

    urls for posts and users added

---
+++ Instagram/users/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from users import views
+
+urlpatterns = [
+
+    path(
+        route='login/',
+        view=views.login_view,
+        name='login'
+    ),
+
+    path(
+        route='logout/',
+        view=views.logout_view,
+        name='logout'
+    ),
+
+    path(
+        route='signup/',
+        view=views.signup,
+        name='signup'
+    ),
+
+    path(
+        route='me/profile/',
+        view=views.update_profile,
+        name='update_profile'
+    )
+
+]
