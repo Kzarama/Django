3d219a23d3e4f060d507712660d87dbf72383c7d
kz <zaramaluna1999@hotmail.com>
2020-06-01 21:20:24 -0500

    project new added

---
+++ instagram/instagram/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path
+from django.conf. urls.static import static
+
+from posts import views as posts_views
+from users import views as users_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', posts_views.list_posts, name='feed'),
+    path('posts/new/', posts_views.create_post, name='create_post'),
+
+    path('users/login/', users_views.login_view, name='login'),
+    path('users/logout/', users_views.logout_view, name='logout'),
+    path('users/signup/', users_views.signup, name='signup'),
+    path('users/me/profile/', users_views.update_profile, name='update_profile'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
