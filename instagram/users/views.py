feef5d5be9c69b471a4d0740663dfdf664209ab2
kz <zaramaluna1999@hotmail.com>
2020-06-02 16:34:02 -0500

    method login and logout changed for use view base class

--- instagram/users/views.py
+++ instagram/users/views.py
@@ -1,13 +1,11 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
-from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, FormView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse, reverse_lazy
 
 from django.contrib.auth.models import User
-from posts.models import Post
 from users.models import Profile
+from posts.models import Post
 
 from users.forms import SignupForm
 
@@ -48,21 +46,8 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         username = self.object.user.username
         return reverse('users:detail', kwargs={'username': username})
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+class LoginView(auth_views.LoginView):
+    template_name = 'users/login.html'
 
-        user = authenticate(request, username=username, password=password)
-
-        if user:
-            login(request, user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
-    return render(request, 'users/login.html')
-
-@login_required
-def logout_view(request):
-    logout(request)
-    return redirect('users:login')
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    template_name = 'users/logged_out.html'
