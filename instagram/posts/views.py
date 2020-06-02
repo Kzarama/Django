8013a50ed5d105ae69604bfbf816359f6902fa32
kz <zaramaluna1999@hotmail.com>
2020-06-02 13:40:06 -0500

    postdetail and create post added

--- instagram/posts/views.py
+++ instagram/posts/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 
 from posts.forms import PostForm
 
@@ -14,21 +13,18 @@ class PostsFeedView(LoginRequiredMixin, ListView):
     paginate_by = 2
     context_object_name = 'posts'
 
-@login_required
-def create_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
-    return render(
-        request=request,
-        template_name='posts/new.html',
-        context={
-            'form': form,
-            'user': request.user,
-            'profile': request.user.profile,
-        }
-    )
+class PostDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'posts/detail.html'
+    queryset = Post.objects.all()
+    context_objext_name = 'post'
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
